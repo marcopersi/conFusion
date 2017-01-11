fd48f17b263859bfc0d08d454c63713869b4189b
Marco Persi <persi.marco@gmail.com>
2017-01-11 21:46:12 +0100

    added npm load script

---
+++ conFusion/npmload.sh
@@ -0,0 +1,18 @@
+#! bin/sh
+
+sudo npm install gulp --save-dev
+sudo npm install gulp-minify-css --save-dev
+sudo npm install gulp-jshint --save-dev
+sudo npm install cli --save-dev
+sudo npm install del --save-dev
+sudo npm install jshint --save-dev
+sudo npm install jshint-stylish --save-dev
+sudo npm install gulp-uglify --save-dev
+sudo npm install gulp-usemin --save-dev
+sudo npm install gulp-imagemin --save-dev
+sudo npm install gulp-notify --save-dev
+sudo npm install gulp-cache --save-dev
+sudo npm install gulp-changed --save-dev
+sudo npm install gulp-rev --save-dev
+sudo npm install browser-sync --save-dev
+sudo npm install gulp-ng-annotate --save-dev
